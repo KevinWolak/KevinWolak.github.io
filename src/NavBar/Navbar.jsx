ea43cfa413af57b5123ced263638177c5a6b28e2
kevin <kevin_wolak@hotmail.com>
2023-04-06 18:27:59 -0400

    unrolled changes

--- src/NavBar/Navbar.jsx
+++ src/NavBar/Navbar.jsx
@@ -4,35 +4,35 @@ import { Link, useMatch, useResolvedPath } from "react-router-dom";
 import { FaReact } from "react-icons/fa";
 
 const Navbar = () => {
-  let clickedClass = "clicked";
-  const body = document.body;
-  const lightTheme = "light";
-  const darkTheme = "dark";
-  let theme;
+  const [darkThemeEnabled, setDarkThemeEnabled] = useState(
+    localStorage.getItem("darkThemeEnabled") === "true"
+  );
+  const toggleButtonRef = useRef(null);
 
-  if (localStorage) {
-    theme = localStorage.getItem("theme");
+  function toggleTheme() {
+    const newDarkThemeEnabled = !darkThemeEnabled;
+    document.body.classList.toggle("dark-mode", newDarkThemeEnabled);
+    document
+      .querySelectorAll('link[rel="stylesheet"]')
+      .forEach((item) => (item.disabled = !newDarkThemeEnabled));
+    setDarkThemeEnabled(newDarkThemeEnabled);
   }
 
-  if (theme === lightTheme || theme === darkTheme) {
-    body.classList.add(theme);
-  } else {
-    body.classList.add(lightTheme);
-  }
-
-  const switchTheme = (e) => {
-    if (theme === darkTheme) {
-      body.classList.replace(darkTheme, lightTheme);
-      e.target.classList.remove(clickedClass);
-      localStorage.setItem("theme", "light");
-      theme = lightTheme;
-    } else {
-      body.classList.replace(lightTheme, darkTheme);
-      e.target.classList.add(clickedClass);
-      localStorage.setItem("theme", "dark");
-      theme = darkTheme;
+  useEffect(() => {
+    localStorage.setItem("darkThemeEnabled", darkThemeEnabled);
+    console.log(localStorage.getItem("darkThemeEnabled"));
+    if (
+      localStorage.getItem("darkThemeEnabled") === "true" &&
+      !document.body.classList.contains("dark-mode")
+    ) {
+      document.body.classList.add("dark-mode");
+    } else if (
+      localStorage.getItem("darkThemeEnabled") != "true" &&
+      document.body.classList.contains("dark-mode")
+    ) {
+      document.body.classList.remove("dark-mode");
     }
-  };
+  }, [darkThemeEnabled, setDarkThemeEnabled]);
 
   return (
     <nav id="nav">
@@ -45,10 +45,32 @@ const Navbar = () => {
         <CustomLink to="/contact"> Contact </CustomLink>
       </ul>
       <button
-        className={theme === "dark" ? clickedClass : ""}
-        id="darkMode"
-        onClick={(e) => switchTheme(e)}
-      ></button>
+        className="theme-button"
+        id="dark-mode-toggle"
+        ref={toggleButtonRef}
+        onClick={toggleTheme}
+      >
+        <div>
+          <svg
+            width="20"
+            height="20"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            {darkThemeEnabled ? (
+              <path
+                className="sun "
+                d="M12 17.5a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zm0 1.5a7 7 0 1 0 0-14 7 7 0 0 0 0 14zm12-7a.8.8 0 0 1-.8.8h-2.4a.8.8 0 0 1 0-1.6h2.4a.8.8 0 0 1 .8.8zM4 12a.8.8 0 0 1-.8.8H.8a.8.8 0 0 1 0-1.6h2.5a.8.8 0 0 1 .8.8zm16.5-8.5a.8.8 0 0 1 0 1l-1.8 1.8a.8.8 0 0 1-1-1l1.7-1.8a.8.8 0 0 1 1 0zM6.3 17.7a.8.8 0 0 1 0 1l-1.7 1.8a.8.8 0 1 1-1-1l1.7-1.8a.8.8 0 0 1 1 0zM12 0a.8.8 0 0 1 .8.8v2.5a.8.8 0 0 1-1.6 0V.8A.8.8 0 0 1 12 0zm0 20a.8.8 0 0 1 .8.8v2.4a.8.8 0 0 1-1.6 0v-2.4a.8.8 0 0 1 .8-.8zM3.5 3.5a.8.8 0 0 1 1 0l1.8 1.8a.8.8 0 1 1-1 1L3.5 4.6a.8.8 0 0 1 0-1zm14.2 14.2a.8.8 0 0 1 1 0l1.8 1.7a.8.8 0 0 1-1 1l-1.8-1.7a.8.8 0 0 1 0-1z"
+              />
+            ) : (
+              <path
+                className="moon"
+                d="M16.5 6A10.5 10.5 0 0 1 4.7 16.4 8.5 8.5 0 1 0 16.4 4.7l.1 1.3zm-1.7-2a9 9 0 0 1 .2 2 9 9 0 0 1-11 8.8 9.4 9.4 0 0 1-.8-.3c-.4 0-.8.3-.7.7a10 10 0 0 0 .3.8 10 10 0 0 0 9.2 6 10 10 0 0 0 4-19.2 9.7 9.7 0 0 0-.9-.3c-.3-.1-.7.3-.6.7a9 9 0 0 1 .3.8z"
+              />
+            )}
+          </svg>
+        </div>
+      </button>
     </nav>
   );
 };
