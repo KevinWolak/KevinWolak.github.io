294f77adf67edb3b0c4b4cc04fa16254d73850c7
kevin <kevin_wolak@hotmail.com>
2023-04-13 16:19:42 -0400

    update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -33,54 +33,56 @@ const About = () => {
         <h2 className="detial" onClick={jsonHandler}>
           {"const clickMe = { "}
         </h2>
-        {title ? (
-          <div>
-            <p>{'"basicInfo": {'}</p>
-            <section className="arrayContainer">
-              <p>"firstName": "Kevin",</p>
-              <p>"lastName": "Wolak",</p>
-              <p>"age": 26</p>
-            </section>
-            <p>{"}"},</p>
-            <p>{'"location": {'}</p>
-            <section className="arrayContainer">
-              <p>"city": "London",</p>
-              <p>"province": "Ontario"</p>
-            </section>
-            <p>{"}"},</p>
-            <p>{'"contact": {'}</p>
-            <section className="arrayContainer">
-              <p>"number": "519-694-6925",</p>
-              <p>"email": "kevinwolak96@gmail.com"</p>
-            </section>
-            <p>{'"Passion": {'}</p>
-            <section className="arrayContainer">
-              <p>{'"Hobbies": ['}</p>
-              <p className="languages">
-                {" "}
-                "Code", "Chess", "Golf", "Baseketball"
-              </p>
-              <p>{"]"}</p>
-            </section>
-            <p>{"}"},</p>
-            <p>{'"skills": {'}</p>
-            <section className="arrayContainer">
-              <p>{'"languanges": ['}</p>
-              <p className="languages">
-                "CSS", "HTML", "JavaScript", "React.js", "Express.js", "C",
-                "C++", C#, "Cobol"
-              </p>
-              <p>{"]"},</p>
-              <p>{'"Technologies": ['}</p>
-              <p className="languages">
-                "Postman", "Git", "GitHub", "Bootstrap", "TailWind"
-              </p>
-              <p>{"]"}</p>
-            </section>
-            <p className="indentation">{"}"}</p>
-            <h2>{"}"} </h2>
-          </div>
-        ) : null}
+        <div className={title ? "json-object show animate" : "json-object"}>
+          {title ? (
+            <div>
+              <p>{'"basicInfo": {'}</p>
+              <section className="arrayContainer">
+                <p>"firstName": "Kevin",</p>
+                <p>"lastName": "Wolak",</p>
+                <p>"age": 26</p>
+              </section>
+              <p>{"}"},</p>
+              <p>{'"location": {'}</p>
+              <section className="arrayContainer">
+                <p>"city": "London",</p>
+                <p>"province": "Ontario"</p>
+              </section>
+              <p>{"}"},</p>
+              <p>{'"contact": {'}</p>
+              <section className="arrayContainer">
+                <p>"number": "519-694-6925",</p>
+                <p>"email": "kevinwolak96@gmail.com"</p>
+              </section>
+              <p>{'"Passion": {'}</p>
+              <section className="arrayContainer">
+                <p>{'"Hobbies": ['}</p>
+                <p className="languages">
+                  {" "}
+                  "Code", "Chess", "Golf", "Baseketball"
+                </p>
+                <p>{"]"}</p>
+              </section>
+              <p>{"}"},</p>
+              <p>{'"skills": {'}</p>
+              <section className="arrayContainer">
+                <p>{'"languanges": ['}</p>
+                <p className="languages">
+                  "CSS", "HTML", "JavaScript", "React.js", "Express.js", "C",
+                  "C++", C#, "Cobol"
+                </p>
+                <p>{"]"},</p>
+                <p>{'"Technologies": ['}</p>
+                <p className="languages">
+                  "Postman", "Git", "GitHub", "Bootstrap", "TailWind"
+                </p>
+                <p>{"]"}</p>
+              </section>
+              <p className="indentation">{"}"}</p>
+              <h2>{"}"} </h2>
+            </div>
+          ) : null}
+        </div>
       </section>{" "}
     </>
   );
